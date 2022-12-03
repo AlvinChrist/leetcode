f59e34a439ed4f42bce7443bd499479cf1075ffc
AlvinChrist <alvin.ardiansyah@pt-ssss.com>
2022-12-03 11:05:34 +0700

    .

---
+++ first-unique-character-in-string.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <algorithm>
+#include <cstring>
+#include <cmath>
+
+using namespace std;
+typedef unordered_map<char,int> umpci;
